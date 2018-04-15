af3a8fa054351ac8a83893194dbd6920df8390be
rxy242486 <rxy242486@gmail.com>
2018-04-15 21:28:19 +0800

    List

--- List/LinkList/LinkList.cpp
+++ List/LinkList/LinkList.cpp
@@ -1,11 +1,25 @@
 #include "LinkList.h"
 #include "iostream"
 using namespace std;
+
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 int main(int argc, char** argv) {
 	LinkList L;
+	ElemType e;
 	CreateList_L(L,5);
 	PrintList_L(L);
+	cout<<"线性表长度："<<Length_L(L)<<endl;
+	GetElem_L(L,2,e);
+	cout<<"第二个元素为:"<<e<<endl;
+	cout<<LocateElem_L(L,4)<<endl;
+	cout<<"==============插入测试==============="<<endl;
+	ListInsert_L(L,3,9);
+	PrintList_L(L);
+	cout<<"==============删除测试==============="<<endl;
+	ListDelete_L(L,3,e);
+	cout<<"删除的元素为："<<e<<endl;
+	PrintList_L(L); 
 	return 0;
 }
 void CreateList_L(LinkList &L,int n){
@@ -38,3 +52,92 @@ void PrintList_L(LinkList &L){
 		p = p->next;
 	}
 }
+Status GetElem_L(LinkList L,int i,ElemType &e){
+	if(i<1||i>L->data)//判断i的合法性 
+		return ERROR;
+	else{
+		LinkList p = L->next;
+		for(int j = 1;j<i;j++)
+		{
+			if(p){
+				p = p->next;
+			}
+			else
+				break;
+		}
+		e = p->data;
+		return OK;
+	}		
+}
+int Length_L(LinkList &L)
+{
+	return L->data;
+}
+int LocateElem_L(LinkList &L,ElemType e)
+{
+	LinkList p = L->next;
+	int i = 1;
+	while(p&&p->data!=e&&(i+1)<=L->data)
+	{
+		i++;
+		p = p->next;
+	}
+	if(i+1>L->data)
+	{
+		return 0;//没找到 
+	}
+	else
+		return i;//找到了，返回其位序 
+}
+Status ListInsert_L(LinkList &L,int i,ElemType e){
+	if(i<1||i>L->data)//判断i的合法性 
+		return ERROR;
+	else{
+		LinkList p = L->next;
+		for(int j = 1;j<i-1;j++)
+		{
+			if(p){
+				p = p->next;
+			}
+			else
+				break;
+		}
+		LinkList q = new LNode;
+		q->next = p->next;
+		p->next = q;
+		q->data = e;
+		return OK;
+		
+	} 
+}
+Status ListDelete_L(LinkList &L,int i,ElemType &e){
+	if(i<1||i>L->data)//判断i的合法性 
+		return ERROR;
+	else{
+		LinkList p = L->next;
+		for(int j = 1;j<i-1;j++)
+		{
+			if(p){
+				p = p->next;
+			}
+			else
+				break;
+		}
+		e = p->next->data;
+		p->next = p->next->next;
+		return OK;
+		
+	} 
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- List/LinkList/LinkList.h
+++ List/LinkList/LinkList.h
@@ -10,17 +10,22 @@ typedef struct LNode{
 //逆序输入n个元素的值，建立带表头结点的单链线性表L
 void CreateList_L(LinkList &L,int n); 
 //L为带头结点的单链表的头指针
-//当第i个元素存在时，其值赋给e并返回OK，否则返回ERROR 
+//按位查找操作，当第i个元素存在时，其值赋给e并返回OK，否则返回ERROR 
 Status GetElem_L(LinkList L,int i,ElemType &e);
 //在带头结点的单链线性表L中第i个位置之前插入元素e
 Status ListInsert_L(LinkList &L,int i,ElemType e);
 //在带头结点的单链线性表L中，删除第i个元素，并由e返回其值
 Status ListDelete_L(LinkList &L,int i,ElemType &e);
-//已知单链线性表La和Lb的元素按值非递减排列。
-//归并La和Lb得到新的单链线性表Lc，Lc的元素也按值非递减排列。
-void MergeList_L(LinkList &La,LinkList &Lb,LinkList &Lc); 
 //从头打印链表
 void PrintList_L(LinkList &L); 
+//求表长
+int Length_L(LinkList &L);
+//按值查找操作。在表L中查找具有给定关键字值的元素
+int LocateElem_L(LinkList &L,ElemType e);
+//判空操作。若表L为空表，则返回OK，否则返回ERROR
+Status Empty_L(LinkList &L);
+//销毁操作。销毁线性表，并释放线性表L所占用的内存空间
+void Destroy_L(LinkList &L); 
 
 
 
